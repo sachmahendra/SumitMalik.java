5251105fd4f175842505b6ead3d8013b39cdd6e6
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-07-01 17:33:31 +0530

    initial.commit
    IT IS A JAVA CODE FOR THE IMPLEMENTATION OF PATTERN CODE

---
+++ L42Pattern.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class L42Pattern {
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        int n= scn.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=n;j>i-1;j--){
+               System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
