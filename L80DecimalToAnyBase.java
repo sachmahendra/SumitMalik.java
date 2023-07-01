22f622fea6be2a92182eb877e0502eb401dfd9f3
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-07-01 17:35:15 +0530

    initial.commit
    IT IS A JAVA JAVA CODE FOR CONVERTING DECIMAL TO ANY BASE NUMBER

---
+++ L80DecimalToAnyBase.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class L80DecimalToAnyBase {
+    public static int getValueInBase(int n, int b){
+        int rv=0;
+        int p=1;
+        while(n > 0){
+            int dig= n%b;
+            n=n/b;
+
+             rv=rv+(dig*p);
+             p=p*10;
+        }
+        return rv;
+
+    }
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        int n = scn.nextInt();
+        int b= scn.nextInt();
+        System.out.println(getValueInBase(n, b));
+    }
+}
