30074e465bb71fcb3e352e6b7feb32ff60302dff
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-08-21 19:38:52 +0530

    initial.commit
    IT IS THE SIMPLE CHANGES IN THE CODE OF TRANGLE OF THE PATTERN

--- L35Tringle2.java
+++ L35Tringle2.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class L35Tringle2 {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
